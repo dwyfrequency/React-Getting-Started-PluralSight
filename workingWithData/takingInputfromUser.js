e852671f2fb7cd4487f90d931e0e1e17ae0e9a7b
dwyfrequency <jdwy215@gmail.com>
2018-09-09 06:01:57 -0400

    adding submit event

--- workingWithData/takingInputfromUser.js
+++ workingWithData/takingInputfromUser.js
@@ -2,19 +2,6 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const data = [
-  {
-    name: "Johnny Tsunami",
-    avatar_url: "https://avatars3.githubusercontent.com/u/8900894?v=4",
-    company: "Good Burger"
-  },
-  {
-    name: "Jannie Tsunami",
-    avatar_url: "https://avatars3.githubusercontent.com/u/8900894?v=4",
-    company: "Mondo Burger"
-  }
-];
-
 const Card = props => {
   return (
     <div>
@@ -33,7 +20,6 @@ const CardList = props => {
   // by using the spread operator, we are passing all of the data in the object to the component
   return (
     <div>
-      <Form />
       {props.cards.map(x => (
         <Card {...x} />
       ))}
@@ -42,14 +28,46 @@ const CardList = props => {
 };
 
 class Form extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("Event: Form Submit");
+  };
+
   render() {
+    // we want to read the value in the form everytime we submit
     return (
-      <form>
-        <input type="text" placeholder="Github username" />
+      <form onSubmit={() => console.log()}>
+        <input type="text" placeholder="Github username" required />
         <button type="submit">Add card</button>
       </form>
     );
   }
 }
 
-ReactDOM.render(<CardList cards={data} />, document.getElementById("root"));
+class App extends Component {
+  state = {
+    cards: [
+      {
+        name: "Johnny Tsunami",
+        avatar_url: "https://avatars3.githubusercontent.com/u/8900894?v=4",
+        company: "Good Burger"
+      },
+      {
+        name: "Jannie Tsunami",
+        avatar_url: "https://avatars3.githubusercontent.com/u/8900894?v=4",
+        company: "Mondo Burger"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div>
+        <Form />
+        <CardList cards={this.state.cards} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
